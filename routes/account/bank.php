6850f2521951aae9183131809ba53969e50bda6f
Israel Casillas Jr <ICJR@users.noreply.github.com>
2021-07-02 05:07:29 -0700

    feat: add bank deposit history

---
+++ routes/account/bank.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Middleware;
+use App\Http\Actions\{Commands, Web};
+use Contracts\Routing\{Router, RouteMiddleware};
+
+$r->group(
+    $r->name('bank.')
+      ->pattern('/bank'),
+    function (Router $r) {
+        $r->get('deposits', '/deposits[/{page:int}]', Web\Account\Bank\Action::class, 'deposits');
+
+        $r->post('deposit.command', '/deposit', Commands\User\Bank\Deposit\Action::class)
+          ->middleware(function(RouteMiddleware $m) {
+              $m->push(Middleware\Redirect\IfGuest::class);
+          });
+
+        $r->group(
+            $r->name('square.')
+              ->pattern('/square'),
+            function (Router $r) {
+                $r->get('webhook.command', '/webhook', Commands\User\Bank\Deposit\Square\Confirm\Action::class);
+            }
+        );
+
+        $r->group(
+            $r->name('paypal.')
+              ->pattern('/paypal'),
+            function (Router $r) {
+                $r->post('webhook.command', '/webhook', Commands\User\Bank\Deposit\Paypal\IPN\Action::class)
+                  ->middleware(function(RouteMiddleware $m) {
+                      $m->replace([Middleware\Guard\ForceHttps::class]);
+                  });
+            }
+        );
+    }
+);
