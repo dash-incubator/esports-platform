a2ca115e07827f61ecbc88a830c831ce993aed5b
Israel Casillas Jr <ICJR@users.noreply.github.com>
2021-07-28 23:17:45 -0700

    feat: add dash deposits, crud games, and crud platforms

--- resources/views/components/account/modals/bank-deposit.php
+++ resources/views/components/account/modals/bank-deposit.php
@@ -6,7 +6,7 @@
                 <h2 class="page-header-title">Deposit Funds</h2>
             </div>
 
-            <form action="<?= $app['route']->uri('account.bank.deposit.command') ?>" data-submit='processing' method="post">
+            <form action="<?= $app['route']->uri('account.bank.deposit.command') ?>" class="frame --active" id="frame-bank-paypal" data-submit='processing' method="post">
                 <?= $include('@components/field/input/default', [
                     'field' => [
                         'class' => 'field--primary field--white --margin-top-large --width-full'
@@ -45,7 +45,41 @@
                         Checkout With <b>PayPal</b>
                     </div>
                 </button>
+
+                <div class="button button--large button--secondary --margin-top --width-full" data-click='frame' data-frame='bank-dash'>
+                    <div>
+                        Checkout With <b>Dash</b>
+                    </div>
+                </div>
             </form>
+            
+            <div class="frame" id="frame-bank-dash">
+                <div id='qr'></div>
+                
+                <?= $include('@components/field/input/default', [
+                    'field' => [
+                        'class' => 'field--primary field--white --margin-top-large --width-full'
+                    ],
+                    'field-description' => [
+                        'text' => '$' . number_format($app['config']->get('bank.deposit.minimum'), 2) . ' minimum'
+                    ],
+                    'field-tag' => [
+                        'attributes' => [
+                            'min' => $app['config']->get('bank.deposit.minimum'),
+                            'name' => 'amount',
+                            'required' => true,
+                            'step' => '0.01',
+                            'type' => 'number'
+                        ],
+                        'directives' => [
+                            'keyup' => 'bank-deposit-dash'
+                        ]
+                    ],
+                    'field-title' => [
+                        'text' => 'Deposit Amount'
+                    ]
+                ]) ?>
+            </div>
         </div>
 
     </div>
